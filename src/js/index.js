93db6d1148c3f4b77f3288d733ad6284915824f1
Bartlomiej Wanat <bartekk195@gmail.com>
2020-06-23 17:18:39 +0200

    counter ready

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,40 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+
+const addButton = document.querySelector(".button__container--plus-image");
+const deleteButton = document.querySelector(".button__container--minus-image");
+const counter = document.querySelector('.counter--js');
+
+
+let numberOfGlass = 0;
+
+
+const addGlass = function () {
+ numberOfGlass += 200;
+ counter.innerHTML = numberOfGlass
+};
+
+const deleteGlass = function () {
+ if (numberOfGlass > 0) {
+  numberOfGlass -= 200;
+  counter.innerHTML = numberOfGlass;
+ } else {
+  numberOfGlass = 0;
+ }
+};
+
+
+
+
+addButton.addEventListener("click", addGlass);
+deleteButton.addEventListener("click", deleteGlass);
+
+
+
+console.log(counter);
+
+
+
+console.log("lalalal");
